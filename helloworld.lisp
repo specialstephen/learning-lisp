a238f8ad2b111d7c174086cb326a8b2fbba1fa60
jasonb <jason.brownlee05@gmail.com>
2009-09-06 15:36:15 +1000

    updated git ignore

--- helloworld.lisp
+++ helloworld.lisp
@@ -1,1 +1,15 @@
-(+ 1 2)
+; Essential Commands Overview: 
+
+; open slime: c-x slime
+; split windows: control-x 2
+; execute expression: control-escape-x
+; send expression to lisp: escape-control-k
+; load a lisp file from repl: (load "filename.lisp")
+; open file control-x control-f
+; save: control-x s
+; move to other window: control-x o
+
+; some example code
+
+(+ 1 2 3 4 5)
+
